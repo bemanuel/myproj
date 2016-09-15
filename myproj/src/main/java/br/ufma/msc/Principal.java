8a2f10070b9cd83d412ede4a809c4b82d873de4e
Bruno Emanuel de Andrade Silva <bemanuel@agespisa.com.br>
2016-09-14 22:06:49 -0300

    Criação de Tela

--- myproj/src/main/java/br/ufma/msc/Principal.java
+++ myproj/src/main/java/br/ufma/msc/Principal.java
@@ -3,6 +3,10 @@
  */
 package br.ufma.msc;
 
+import java.util.Scanner;
+
+import br.ufma.msc.gui.Tela;
+
 /**
  * @author jwalker
  *
@@ -14,8 +18,28 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		// TODO Receberá as instruções, tela de console com o Menu de opções
-		
+		  Tela tela = new Tela();
+		    tela.menu();
 
+		    Scanner scanner = new Scanner(System.in);
+		    int choice = scanner.nextInt();
+
+		    switch (choice) {
+		        case 1:
+
+		            break;
+		        case 2:
+
+		            break;
+		        case 3:
+
+		            break;
+		        case 4:
+
+		            break;
+		        default:
+		            // The user input an unexpected choice.
+		    }
 	}
 
 }
---
+++ myproj/src/main/java/br/ufma/msc/gui/Tela.java
@@ -0,0 +1,11 @@
+package br.ufma.msc.gui;
+
+public class Tela {
+
+	public void menu() {
+		// TODO Auto-generated method stub
+		System.out.println("1. Criar projeto ");
+		System.out.println("2. Entrar em projeto existente ");
+	}
+	
+}
